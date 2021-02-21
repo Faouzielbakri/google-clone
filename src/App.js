73421100c133e0a9744e8175baa6168c636ae0b3
Faouzi Elbakri <56311060+Faouzielbakri@users.noreply.github.com>
2021-02-21 06:21:51 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 import Home from './pages/Home';
 import { BrowserRouter as Router,Switch,Route } from "react-router-dom";
 import SearchPage from './pages/SearchPage';
